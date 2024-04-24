c83ec9c171ca3e9865e6f876afb1442933788d53
caulkenstein <167827920+caulkenstein@users.noreply.github.com>
2024-04-24 19:23:52 -0400

    added cookie support to mute state

--- main.js
+++ main.js
@@ -10,17 +10,24 @@ function handleAudioButtonClick() {
   if (backgroundAudio.muted) {
     backgroundAudio.muted = false;
     audioButton.classList.remove('muted');
+    localStorage.setItem('audioMuted', 'false');
+    console.log('Audio unmuted. localStorage set to:', localStorage.getItem('audioMuted'));
   } else {
     backgroundAudio.muted = true;
     audioButton.classList.add('muted');
+    localStorage.setItem('audioMuted', 'true');
+    console.log('Audio muted. localStorage set to:', localStorage.getItem('audioMuted'));
   }
 }
 
 // Add click event listener to the audio button
 audioButton.addEventListener('click', handleAudioButtonClick);
 
-// Mute the audio initially if the browser is Chrome
-if (isChrome) {
+// Check if the audio muted value exists in localStorage
+var audioMutedValue = localStorage.getItem('audioMuted');
+
+// Mute the audio initially if the browser is Chrome or if the audio muted value is set to 'true'
+if (isChrome || audioMutedValue === 'true') {
   backgroundAudio.muted = true;
   audioButton.classList.add('muted');
 }
