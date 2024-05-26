99e4ab62bf39138769e18d6cfca1b45ff1ea698a
caulkenstein <167827920+caulkenstein@users.noreply.github.com>
2024-05-26 12:16:52 -0400

    Fixed audio autoplay issues

--- main.js
+++ main.js
@@ -1,22 +1,19 @@
-// Check if the browser is Chrome
-var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-
 // Get the necessary elements
 var audioButton = document.querySelector('.audio-btn');
 var backgroundAudio = document.getElementById('backgroundAudio');
 
 // Function to handle the audio button click event
 function handleAudioButtonClick() {
-  if (backgroundAudio.muted) {
-    backgroundAudio.muted = false;
+  if (backgroundAudio.paused) {
+    backgroundAudio.play();
     audioButton.classList.remove('muted');
     localStorage.setItem('audioMuted', 'false');
-    console.log('Audio unmuted. localStorage set to:', localStorage.getItem('audioMuted'));
+    console.log('Audio played. localStorage set to:', localStorage.getItem('audioMuted'));
   } else {
-    backgroundAudio.muted = true;
+    backgroundAudio.pause();
     audioButton.classList.add('muted');
     localStorage.setItem('audioMuted', 'true');
-    console.log('Audio muted. localStorage set to:', localStorage.getItem('audioMuted'));
+    console.log('Audio paused. localStorage set to:', localStorage.getItem('audioMuted'));
   }
 }
 
@@ -26,8 +23,11 @@ audioButton.addEventListener('click', handleAudioButtonClick);
 // Check if the audio muted value exists in localStorage
 var audioMutedValue = localStorage.getItem('audioMuted');
 
-// Mute the audio initially if the browser is Chrome or if the audio muted value is set to 'true'
-if (isChrome || audioMutedValue === 'true') {
-  backgroundAudio.muted = true;
+// Pause the audio initially if the audio muted value is set to 'true'
+if (audioMutedValue === 'true') {
+  backgroundAudio.pause();
   audioButton.classList.add('muted');
+} else {
+  backgroundAudio.play();
+  audioButton.classList.remove('muted');
 }
